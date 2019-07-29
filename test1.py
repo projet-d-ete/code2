8dd344bdbe0d791647a78385b3a6eb4bf5841387
basmabensalem <53427647+basmabensalem@users.noreply.github.com>
2019-07-29 12:52:11 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,9 +2,9 @@ from PIL import Image
 import numpy as np
 import cv2
 import math
-imgpil=Image.open("obj94__0.png")
-imgpil2=Image.open("obj94__5.png")
-imgpil3=Image.open("obj94__10.png")
+imgpil=Image.open("nn.png")
+imgpil2=Image.open("mm.png")
+imgpil3=Image.open("hh.png")
 imgpil.show()
 imgpil2.show()
 imgpil3.show()
@@ -20,32 +20,31 @@ img3=np.array(img000)
 #pour la 1ère image
 print("pour la 1ére image:")
 moments=cv2.moments(img)
-print(moments)
+print("les moments :",moments)
 huMoments=cv2.HuMoments(moments)
-print(huMoments)
+print("\n")
 for i in range(0,7):
     huMoments[i]=-1*math.copysign(1.0,huMoments[i])*math.log10(abs(huMoments[i]))
-print(huMoments)
+print("les humoments:",huMoments)
 print("\n")
 
 #pour la 2ème image
 print("pour la 2eme image:")
 moments=cv2.moments(img2)
-print(moments)
+print("les moments :",moments)
 huMoments=cv2.HuMoments(moments)
-print(huMoments)
+print("\n")
 for i in range(0,7):
     huMoments[i]=-1*math.copysign(1.0,huMoments[i])*math.log10(abs(huMoments[i]))
-print(huMoments)
+print("les humoments:",huMoments)
 print("\n")
 
 #pour la 3ème image
 print("pour la 3eme image:")
 moments=cv2.moments(img3)
-print(moments)
+print("les moments :",moments)
 huMoments=cv2.HuMoments(moments)
-print(huMoments)
+print("\n")
 for i in range(0,7):
     huMoments[i]=-1*math.copysign(1.0,huMoments[i])*math.log10(abs(huMoments[i]))
-print(huMoments)
-print("\n")
+print("les humoments:",huMoments)
